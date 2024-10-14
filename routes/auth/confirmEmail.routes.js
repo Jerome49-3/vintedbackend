7665f97924286c4b16b0ec4bedf60552d4e95a8b
Jerome Bobin <jeromebobin49.3@gmail.com>
2024-10-14 15:36:43 +0200

    const { code } = req.body;

--- routes/auth/confirmEmail.routes.js
+++ routes/auth/confirmEmail.routes.js
@@ -7,32 +7,36 @@ const fileUpload = require("express-fileupload");
 //models
 const User = require("../../models/User");
 
-router.get("/confirmemail/:code", fileUpload(), async (req, res) => {
+router.get("/confirmemail", fileUpload(), async (req, res) => {
   console.log("je suis sur la route /confirmemail");
-  const code = req.params.code;
+  const { code } = req.body;
+  console.log("code:", code);
+
   if (code) {
     const user = await User.findOne({ code: code });
     console.log("code in /confirmemail:", code);
     console.log("user in /confirmemail:", user);
-    if (user && code === user.code) {
-      const token = jwt.sign(
-        {
-          _id: user.id,
-          account: user.account,
-          isAdmin: user.isAdmin,
-          newsletter: user.newsletter,
-        },
-        process.env.SRV_KEY_SECRET
-      );
-      user.emailIsConfirmed = true;
-      user.token = token;
-      await user.save();
+    if (user) {
+      if (code === user.code) {
+        const token = jwt.sign(
+          {
+            _id: user.id,
+            account: user.account,
+            isAdmin: user.isAdmin,
+            newsletter: user.newsletter,
+          },
+          process.env.SRV_KEY_SECRET
+        );
+        user.emailIsConfirmed = true;
+        user.token = token;
+        await user.save();
 
-      return res.status(200).json({
-        message:
-          "Merci votre email est bien confirmé, vous aller être redirigé vers la route /publish",
-        token,
-      });
+        return res.status(200).json({
+          message:
+            "Merci votre email est bien confirmé, vous aller être redirigé vers la route /publish",
+          token,
+        });
+      }
     }
   } else {
     return res.status(400).json({ message: "Oups, somthing went wrong" });
